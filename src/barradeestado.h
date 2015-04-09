df5f4d9642a12aa1a731ca429882dc8dd11247ac
Carsten Teibes <dev@f4ke.de>
2015-04-09 20:02:16 +0200

    wipe all the files _o/

--- src/barradeestado.h
+++
@@ -1,4 +0,0 @@
-/* barradeestado.h */
-/* Header para barraestado.c */
-
-void barradeestado (SDL_Surface *ventana, SDL_Surface *tiles, int habitacion, int vidas, int cruces, TT_Font *fuente);
--- src/fase.h
+++
@@ -1,5 +0,0 @@
-/* fase.h */
-/* Header para fase.c */
-
-void dibujarfase (SDL_Surface *ventana, int fase[][22][32], SDL_Surface *tiles, int habitacion, int ciclo[], int cambiot, SDL_Surface *fondomd, Mix_Chunk *fx[]);
-void animitems (int fase[][22][32], int habitacion, int ciclo[]);
